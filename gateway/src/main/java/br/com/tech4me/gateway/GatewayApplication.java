c8b99abe8aa9d86f39098b13a30fa7e1f94cbc09
AnaDamiao <damiaoclara2003@gmail.com>
2023-01-20 19:25:22 -0300

    ajustando o Gateway

--- gateway/src/main/java/br/com/tech4me/gateway/GatewayApplication.java
+++ gateway/src/main/java/br/com/tech4me/gateway/GatewayApplication.java
@@ -2,9 +2,11 @@ package br.com.tech4me.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableEurekaServer
+@EnableDiscoveryClient
+
 public class GatewayApplication {
 
 	public static void main(String[] args) {
