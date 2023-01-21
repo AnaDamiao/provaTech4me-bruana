c65e52e0dc52b65c62904162adea131c6454da35
Brunasouza1 <brunaisabelle123@outlook.com.br>
2023-01-21 16:36:52 -0300

    (adição do atendimento)

---
+++ atendimento/src/main/java/br/com/tech4meroupas/atendimento/AtendimentoApplication.java
@@ -0,0 +1,13 @@
+package br.com.tech4meroupas.atendimento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtendimentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtendimentoApplication.class, args);
+	}
+
+}
---
+++ atendimento/src/test/java/br/com/tech4meroupas/atendimento/AtendimentoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tech4meroupas.atendimento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtendimentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
