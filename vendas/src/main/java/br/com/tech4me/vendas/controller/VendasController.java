9429493e3783078c2d982b6d82338982854251b3
Brunasouza1 <brunaisabelle123@outlook.com.br>
2023-02-05 12:44:55 -0300

    (atualização controller)

--- vendas/src/main/java/br/com/tech4me/vendas/controller/VendasController.java
+++ vendas/src/main/java/br/com/tech4me/vendas/controller/VendasController.java
@@ -1,12 +1,9 @@
 package br.com.tech4me.vendas.controller;
 
 import java.util.Optional;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,10 +12,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.github.andrewoma.dexx.collection.List;
-
-import br.com.tech4me.vendas.model.Venda;
 import br.com.tech4me.vendas.service.VendaService;
 import br.com.tech4me.vendas.shared.VendaCompletoDto;
 import br.com.tech4me.vendas.shared.VendaDto;
@@ -29,7 +22,7 @@ import jakarta.validation.Valid;
 public class VendasController {
 
     @Autowired
-    private VendaService vender;
+    public VendaService vender;
 
     //cadastrando venda
    @PostMapping
@@ -39,14 +32,8 @@ public class VendasController {
     return new ResponseEntity<>(vender.CadastrarVenda(venda),HttpStatus.CREATED);
    }
  
-   //buscando venda
-   @GetMapping
-   public ResponseEntity<List<VendaCompletoDto>> obterVenda()
-   {
-       return new ResponseEntity<>(vender.obterVenda(),HttpStatus.OK);
-   } 
-   
-   //Buscar por id
+  
+    //Buscar por id
    @GetMapping("/{id}")
 
    public ResponseEntity<VendaDto> obterVendaPorId(@PathVariable String id)
